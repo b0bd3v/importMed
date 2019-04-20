7e67b7b4b0dbd9534c0608abed551a501cd95bcd
Roberto Martins <roberto.martins.info@gmail.com>
2019-04-20 00:00:18 -0300

    Ajustes de layout.

--- resources/views/file/processedData.blade.php
+++ resources/views/file/processedData.blade.php
@@ -6,8 +6,12 @@
         <tbody>
             @foreach($files as $file)
             <tr>
-                <td>{{ $file }}</td>
-                <td style="width: 10px"><a href="{{route('view-file', ['name' => $file])}}" target="_blank">Abrir</a></td>
+                <td>
+                    <span>{{ $file }}</span>
+                </td>
+                <td style="width: 116px">
+                    <a class="btn btn-primary" href="{{route('view-file', ['name' => $file])}}" target="_blank"><i class="fa fa-download"></i> Baixar</a>
+                </td>
             </tr>
             @endforeach
         </tbody>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,7 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{config('app.name','LSAPP')}}</title>
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 </head>
 <body>
+    <nav class="navbar navbar-light bg-light">
+        <span class="navbar-brand mb-0 h1">Indicadores</span>
+    </nav>
        @yield('content')
 </body>
